c331b2703048490ae953048c3cbb291c52b46b70
Planet-X <planetx.dev@gmail.com>
2018-01-05 19:50:51 +0100

    msm_hotplug: Remove cpu_boost dependency
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- arch/arm64/hotplug/msm_hotplug.c
+++ arch/arm64/hotplug/msm_hotplug.c
@@ -339,8 +339,7 @@ static void cpu_down_work(struct work_struct *work)
 			continue;
 		lowest_cpu = get_lowest_load_cpu();
 		if (lowest_cpu > 0 && lowest_cpu <= stats.total_cpus) {
-			if (check_lock(lowest_cpu) ||
-			    check_cpuboost(lowest_cpu))
+			if (check_lock(lowest_cpu))
 				break;
 			cpu_down(lowest_cpu);
 		}
@@ -524,11 +523,7 @@ static void msm_hotplug_resume(void)
 		}
 	}
 
-#ifdef CONFIG_CPU_BOOST
-	if (wakeup_boost || required_wakeup) {
-#else
 	if (required_wakeup) {
-#endif
 		/* Fire up all CPUs */
 		for_each_possible_cpu(cpu) {
 			if (cpu == 0)
